d6df5b363b4aad9a0aaa2a9abab387dac0fa33f7
Masixole Boya <tshollyboya00@gmail.com>
2024-01-21 12:49:42 +0000

    .

---
+++ classesAndObjects.js
@@ -0,0 +1,12 @@
+class Car{
+    constructor(brand, model, ownername, modelyear){
+        this.brand = brand;
+        this.model = model;
+        this.ownerName = ownername;
+        this.modelYear = modelyear;
+
+    }
+}
+
+
+const max = new Car()
