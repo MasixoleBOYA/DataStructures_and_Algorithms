42a3841337f1f39ffa1e0196744adf609efe0319
Masixole Boya <tshollyboya00@gmail.com>
2024-01-21 20:42:29 +0000

    JavaScript classes and objects

--- classesAndObjects.js
+++ classesAndObjects.js
@@ -1,4 +1,4 @@
-class Car{
+class Car {
     constructor(brand, model, ownername, modelyear){
         this.brand = brand;
         this.model = model;
@@ -6,7 +6,14 @@ class Car{
         this.modelYear = modelyear;
 
     }
+    getInfo(){
+        console.log(`The brand name: ${this.brand}`);
+        console.log(`The model name: ${this.model}`);
+        console.log(`The Owner is: ${this.ownerName}`);
+        console.log(`The model year is: ${this.modelYear}`);
+    }
 }
 
 
-const max = new Car()
+const car1 = new Car("Honda","NSX","Max",2015);
+console.log(car1.getInfo());
