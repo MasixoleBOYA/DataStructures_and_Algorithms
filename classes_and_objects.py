40b113b0e5f738c03fa82198744db86fc7e0b0b0
Masixole Boya <tshollyboya00@gmail.com>
2024-02-11 15:03:23 +0000

    classes and objects in python

--- classes_and_objects.py
+++ classes_and_objects.py
@@ -1,14 +1,24 @@
-class Person:
-    def __init__(self, name, surname, age):
-        self.name = name
-        self.surname = surname
-        self.age = age
+if __name__ == "__main__":
+    
+    class Person:
+        def __init__(self, name:str, surname:str, age:int)->None:
+            self.name = name
+            self.surname = surname
+            self.age = age
 
-    def introduce_self(self):
-        print(f"My name is {self.name}")
-        print(f"My surname is {self.surname}")
-        print(f"I am {self.age}")
+        def introduce_self(self)->None:
+            print(f"\nMy name is {self.name}")
+            print(f"My surname is {self.surname}")
+            print(f"I am {self.age}")
+            
 
-guy1 = Person("Masixole", "Boya", 24)
+    guy1 = Person("Masixole", "Boya", 24)
 
-guy1.introduce_self()
+    guy1.introduce_self()
+
+
+
+
+
+
+
